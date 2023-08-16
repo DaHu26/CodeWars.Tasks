138cca27d5eca97534a54d6b4b9a76895bcdfe73
DaHu <plis-danil@mail.ru>
2023-08-16 20:04:11 +0300

    +2 tasks

---
+++ CodeWars/kyu7/SolveTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assert = NUnit.Framework.Assert;
+
+namespace CodeWars.SolveTest
+{
+    public static class Kata
+    {
+        public static int Solve(List<int> arr)
+        {
+            var groupsArr = arr.GroupBy(x  => Math.Abs(x));
+
+            foreach (var group in groupsArr)
+            {
+                var maxValue = group.Max();
+                var minValue = group.Min();
+                if (maxValue == minValue)
+                    return minValue;
+            }
+            return 0;
+
+        }
+    }
+
+    [TestClass]
+    public class SolutionTest
+    {
+        [TestMethod]
+        public void SampleTest()
+        {
+            Assert.That(Kata.Solve(new List<int> { 1, -1, 2, -2, 3 }), Is.EqualTo(3));
+            Assert.That(Kata.Solve(new List<int> { -3, 1, 2, 3, -1, -4, -2 }), Is.EqualTo(-4));
+            Assert.That(Kata.Solve(new List<int> { 1, -1, 2, -2, 3, 3 }), Is.EqualTo(3));
+            Assert.That(Kata.Solve(new List<int> { -110, 110, -38, -38, -62, 62, -38, -38, -38 }), Is.EqualTo(-38));
+            Assert.That(Kata.Solve(new List<int> { -9, -105, -9, -9, -9, -9, 105 }), Is.EqualTo(-9));
+        }
+    }
+}
---
+++ CodeWars/kyu7/SquareDigitsTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.SquareDigitsTest
+{
+    public class Kata
+    {
+        public static int SquareDigits(int n)
+        {
+            var stringInt = n.ToString();
+            char[] charInt = stringInt.ToCharArray();
+            var listInt = new List<int>();
+
+            foreach (char c in charInt) 
+            {
+                var number = c - '0';
+                var square = number * number;
+                listInt.Add(square);
+            }
+
+            var stringResult = String.Join("", listInt);
+            var result = int.Parse(String.Join("", stringResult.Where(x => char.IsDigit(x))));
+
+            return result;
+        }
+    }
+
+        [TestClass]
+    public class KataTest
+    {
+        [TestMethod]
+        public void SquareDigitsTest()
+        {
+            Assert.AreEqual(811181, Kata.SquareDigits(9119));
+            Assert.AreEqual(0, Kata.SquareDigits(0));
+        }
+    }
+}
