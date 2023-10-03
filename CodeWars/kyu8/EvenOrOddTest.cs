9a15750325349f8f0339d8ff5c3b511b335b6717
DaHu <plis-danil@mail.ru>
2023-10-03 18:34:00 +0300

    add 2 kyu8

---
+++ CodeWars/kyu8/EvenOrOddTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.EvenOrOddTest
+{
+    public class EvenOrOddClass
+    {
+        public static string EvenOrOdd(int number)
+        {
+            return number % 2 == 0 ? "Even" : "Odd";
+        }
+    }
+
+    [TestClass]
+    public class TestClass
+    {
+        [TestMethod]
+        [DataRow(12, "Even")]
+        [DataRow(11, "Odd")]
+        [DataRow(101, "Odd")]
+        public void EvenOrOddTest(int number, string expectedResult)
+        {
+            var result = EvenOrOddClass.EvenOrOdd(number);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ CodeWars/kyu8/MakeNegativeTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.MakeNegativeTets
+{
+    public static class Kata
+    {
+        public static int MakeNegative(int number)
+        {
+            return number <= 0 ? number : -number;
+        }
+    }
+
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        [DataRow(5, -5)]
+        [DataRow(0, 0)]
+        [DataRow(-7, -7)]
+        public void MakeNegativeTest(int number, int expectedResult)
+        {
+            var result = Kata.MakeNegative(number);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
