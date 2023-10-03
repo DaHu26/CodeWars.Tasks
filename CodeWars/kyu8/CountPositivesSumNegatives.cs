e3d4a63a23a0a0861c411dd18ba2e81fda3047de
DaHu <plis-danil@mail.ru>
2023-10-03 19:13:34 +0300

    add 1 kyu8

---
+++ CodeWars/kyu8/CountPositivesSumNegatives.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.CountPositivesSumNegatives
+{
+    public class Kata
+    {
+        public static int[] CountPositivesSumNegatives(int[] input)
+        {
+            if (input == null)
+                return new int[0];
+
+            if (input.Length == 0)
+                return new int[0];
+
+            var positiveNumbers = 0;
+            var negativeNumbers = 0;
+
+            foreach (int i in input)
+            {
+                if (i > 0)
+                    positiveNumbers++;
+                else
+                    negativeNumbers += i;
+            }
+            int[] result = new int[] { positiveNumbers, negativeNumbers };
+            return result;
+        }
+    }
+
+    [TestClass]
+    public class TestClass
+    {
+        [TestMethod]
+        [DataRow(new[] { 1, 2, 3, 4, -5, -15 }, new[] { 4, -20 })]
+        [DataRow(new[] { 0, 4, -1, -2, 3}, new[] {2, -3})]
+        public void CountPositivesSumNegativesTest(int[] numbers, int[] expectedResult)
+        {
+            var result = Kata.CountPositivesSumNegatives(numbers);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Assert.AreEqual(expectedResult[i], result[i]);
+            }
+        }
+    }
+}
